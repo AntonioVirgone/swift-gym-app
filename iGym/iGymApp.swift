ac6b5304ad8f15aa3bef2b250745ebc3c8523d52
antonio.virgone <antonio.virgone@wisemotions.com>
2023-03-07 08:49:54 +0100

    Initial Commit

---
+++ iGym/iGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  iGymApp.swift
+//  iGym
+//
+//  Created by Antonio Virgone on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
